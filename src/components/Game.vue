88758ea0cc9c11ae437a9a423d39014659453b51
coyotte508 <coyotte508@gmail.com>
2020-08-01 23:08:56 +0200

    First real commit

---
+++ src/components/Game.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{player}} x {{state}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Engine } from '@boardgamers/powergrid';
+
+@Component
+export default class Game extends Vue {
+  @Prop() state!: Engine;
+  @Prop() player!: number;
+}
+</script>
